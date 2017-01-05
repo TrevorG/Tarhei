0cfe11acb25245dad228e8a1a80549584d394e37
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2017-01-05 15:02:34 +0100

    Non-const getter for object in Packet

--- include/Packet.h
+++ include/Packet.h
@@ -10,6 +10,9 @@ class Packet
 public:
 	Packet(PacketDataType d); //no reference, because we are copying this anyway
 
+	//TODO: consider generating this getter only for PacketDataType
+	//without specific trait
+	PacketDataType& getData();
 	const PacketDataType& get_data() const;
 
 private:
@@ -20,6 +23,12 @@ template <typename DataType>
 Packet<DataType>::Packet(DataType d)
 	: data(d)
 {}
+
+template <typename PacketDataType>
+PacketDataType& Packet<PacketDataType>::getData()
+{
+	return data;
+}
 
 template <typename DataType>
 const DataType& Packet<DataType>::get_data() const
